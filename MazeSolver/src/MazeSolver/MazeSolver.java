bc0c9af44e4c8c83dc8246a5af2ee9cee44ecd1c
morko <morko@LAPTOP-1DI5RSNJ>
2024-11-01 20:56:38 +0100

    day 10 maze solver 2nd try

--- MazeSolver/src/MazeSolver/MazeSolver.java
+++ MazeSolver/src/MazeSolver/MazeSolver.java
@@ -4,12 +4,12 @@ import java.util.LinkedList;
 
 public class MazeSolver {
 	/*		 start of  day 9 		*/
-	static int[][] Maze = {
+/*	static int[][] Maze = {
 			{2, 0, 1, 1},
 			{0, 1, 1, 0},
 			{1, 0, 0, 1},
 			{0, 1, 1, 1}
-	};
+	};*/						//TEMP comment
 	//0 : wall
 	//1 : path
 	//2 : destination
@@ -17,7 +17,7 @@ public class MazeSolver {
 	
 	public static void main(String[] args) {
 		
-		LinkedList<Position> path = new LinkedList<Position>();
+/*		LinkedList<Position> path = new LinkedList<Position>();  //TEMP COMMENT
 
 		Position p = new Position(0, 3); //we use coordinate y for the row and coordinate x for the colomn
 		//  (it looks confusing but it makes sense cause x is for horizontal movement = colomns and y is for vertical movements = rows ) .
@@ -27,7 +27,7 @@ public class MazeSolver {
 
 		Maze[path.peek().y][path.peek().x] = 0;
 		System.out.println("New value : " + Maze[path.peek().y][path.peek().x]);
-
+*/																//TEMP COMMENT
 		/*	 end of  day 9		 */
 		
 		/*	 start of  day 10	 */
@@ -43,9 +43,9 @@ public class MazeSolver {
 		// Left is when x is getting minus one ( x-1 = Left )
 		//How do we do that though ! 
 		//DOWN MOVEMENT :
-		
-		int x;
-		int y;
+/*																TEMP COMMENT		
+		//int x;
+		//int y;
 		
 		//DOWN MOVEMENT :
 		while(true) {
@@ -100,19 +100,110 @@ public class MazeSolver {
 			}
 
 		}
-	/* This code is only capable of finding the right path under the circumstance that it doesn't meet any path that
-	 * is out of boundaries. Or else it throws an exception !  
-	 */
-	
+	/*	//This code is only capable of finding the right path under the circumstance that it doesn't meet any path that
+	 * //is out of boundaries. Or else it throws an exception !  
+	 *	//
+	 *	//
+	 														//TEMP COMMENT
 	/*		 end of day 10 		 */
 		
-	/*		start of day 11		*/
+	/*		start of day 10 2nd try		*/
 	
 	
-	
-	
-	
-	
+		//2ND TRY TO IMPROVE OUR CODING 
+		//We already have the Position class that contains point x and point y but we'll modify it to  " abcisse et ordonnee "
+		
+		int[][] Maze = {
+				{0,0,2,0,1},
+				{0,0,1,0,1},
+				{0,1,0,0,0},
+				{0,1,0,0,1},
+				{0,1,1,1,2},
+				{0,0,0,0,0}
+		};
+		// 0 : wall
+		// 1 : path
+		// 2 : destination ( u win if u find it )
+		
+		LinkedList<Position> path = new LinkedList<Position>(); //Creation of a linked list 
+		int row;
+		int colomn;
+		
+		Position p = new Position(2,3);// we have our starting position at the top right part of the maze.
+		row = p.row;
+		path.push(p);
+		System.out.println("your position is in row : " + p.row); // row = up and down
+		System.out.println("Your position is in colomn : "  + p.colomn); //colomn = left and right
+		System.out.println("Starting position value = " + path.peek().row + " , " + path.peek().colomn);
+		// now we know where we are, we have to assign the starting row and colomn now 
+		System.out.println(Maze[0][0]);
+		row = path.peek().row;	// row assigned ( we can use row instead of typing all that )
+		colomn = path.peek().colomn; // colomn assigned ( we can use colomn instead of typing all that )
+		System.out.println("ooga booga " + Maze[0][4]);
+		// Let's assign the 4 directions : 
+		Maze[row][colomn] = 0;
+
+		while(true) {
+			row = path.peek().row;
+			colomn = path.peek().colomn; 
+			Maze[row][colomn] = 0;
+		//Move Down 
+			if(Maze[row+1][colomn] == 2 ) {
+				System.out.println("Moved Down : YOU WON !");
+				break;
+			}
+			else if (Maze[row+1][colomn] == 1 ) {
+				System.out.println("Moved Down.");
+				path.push(new Position(row+1 , colomn));
+				System.out.println(path.peek().row + " " + path.peek().colomn);
+				//Maze[row+1][colomn] = 0;
+				continue;
+			}
+		//Move Left
+			else if (Maze[row][colomn-1] == 2 ) {
+				System.out.println("Moved Left : YOU WON !");
+				break;
+			}
+			else if (Maze[row][colomn-1] == 1 ) {
+				System.out.println("Moved Left.");
+				path.push(new Position(row , colomn-1));
+				//Maze[row][colomn-1] = 0;
+				continue;
+			}
+		//Move Up
+			else if (Maze[row-1][colomn] == 2 ) {
+				System.out.println("Moved Up : YOU WON !");
+				break;
+			}
+			else if (Maze[row-1][colomn] == 1 ) {
+				System.out.println("Moved Up.");
+				path.push(new Position(row - 1 , colomn));
+				//Maze[row-1][colomn] = 0;
+				continue;
+			}
+		//Move Right
+			else if (Maze[row][colomn+1] == 2) {
+				System.out.println("Moved Right : YOU WON !");
+				break;
+			}
+			else if (Maze[row][colomn+1] == 1 ) {
+				System.out.println("Moved Right.");
+				path.push(new Position(row , colomn + 1));
+				//Maze[row][colomn+1] = 0;
+				continue;
+			}
+	//We got all 4 directions of movement ! let's try them out 		
+			path.pop();
+			if(path.size() <= 0) {
+			System.out.println("No path found ! "); // I still dont understand this part where we have to use the if
+													// statemeny 
+			}
+		}
+		
+		
+		/* end of day 10 2nd try */
+		
+		/* start of day 11 */
 	
 	}
 }
--- MazeSolver/src/MazeSolver/Position.java
+++ MazeSolver/src/MazeSolver/Position.java
@@ -1,11 +1,16 @@
 package MazeSolver;
 
 public class Position {
-	int x;
-	int y;
-	
-	public Position(int y , int x) {
+	//int x;
+	//int y;
+	int row;
+	int colomn;
+	/*public Position(int y , int x) {
 		this.x = x;
 		this.y = y;
-	}
+	}*/
+	public Position(int row,int colomn) {
+		this.row = row;
+		this.colomn = colomn;
+	} // we recreated the same method with different a naming
 }
